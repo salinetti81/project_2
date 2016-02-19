a6535fc94c5ad564fe06825ea345b69ea9231ca9
Daniel Salinetti <dansalinetti@gmail.com>
2016-02-19 11:36:14 -0500

    Set up users model schema, users controller routes, initial steps in Passport set up.

---
+++ models/users.js
@@ -0,0 +1,28 @@
+//userSchema 
+var mongoose = require('mongoose');
+//require second schema here
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema({
+	username: {type: String, required: true, unique: true},
+	email: {String, required: true},
+	password: {String, required: true},
+	bio: String,
+	instruments: String,
+	//add more schema stuff
+});
+
+// methods ======================
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Users', userSchema);
--- server.js
+++ server.js
@@ -1,1 +1,46 @@
-//server.js for project 2
+// set up
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+
+var morgan = require('morgan');
+// var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+// var configDB = require('./config/server.js');
+
+//configuration
+mongoose.connect('mongodb://localhost:27017/users_models');
+
+//express set up
+app.use(morgan('dev'));
+// app.use(cookieParser());
+app.use(bodyParser());
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//passport requirements
+app.use(session({secret: 'secret-session'}));
+app.use(passport.initialize());
+app.use(passport.session()); //for persistent login sessions
+
+//routes
+var usersController = require('./controllers/users');
+//require('./config/passport.js')(passport);
+
+app.use('/users', usersController);
+
+app.get('/', function(req, res) {
+	res.redirect('/users');
+});
+
+app.listen(port, function() {
+	console.log('=======================');
+  console.log('Running on port ' + port);
+  console.log('=======================');
+});
