b2eb266b28d36081b85b1312aee540979a2ea383
Daniel Salinetti <dansalinetti@gmail.com>
2016-02-19 20:11:40 -0500

    Built SoundCloud player widget and added a footer partial.

--- public/js/app.js
+++ public/js/app.js
@@ -9,21 +9,23 @@ console.log("Don't Panic");
 //   client_id: 'YOUR_CLIENT_ID'
 // });
 
+
+// initilizes SoundCloud
 SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4',
   redirect_uri: 'http://external.codecademy.com/soundcloud.html'
 });
 
-
-  SC.get('/tracks', { genres: 'rap' }, function(tracks) {
-    $(tracks).each(function(index, track) {
-      $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
-    });
-  });
-
-
+// lists genres
+  // SC.get('/tracks', { genres: 'jazz	' }, function(tracks) {
+  //   $(tracks).each(function(index, track) {
+  //     $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+  //   });
+  // });
 
 
+
+// Pops up soundcloud login page
   $('a.connect').click(function(e) {
     e.preventDefault();
    SC.connect(function() {
@@ -32,4 +34,11 @@ SC.initialize({
       });
     });
   });
+
+ SC.get('/tracks/293', function(track) {
+  SC.oEmbed(track.permalink_url, document.getElementById('player'));
 });
+
+
+});//ends document.ready
+
